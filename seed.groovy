0d0ea8afda570413f05645b194d11849d4ada701
Mohammed Alam <mohammed.alam@intimetec.com>
2018-07-25 11:23:51 +0530

    job dsl for complete setup

---
+++ seed.groovy
@@ -0,0 +1,84 @@
+import groovy.io.FileType
+import groovy.json.JsonSlurperClassic
+
+def create_pipeline(jobName) {
+
+    def propertiesFile = new JsonSlurperClassic().parseText(readFileFromWorkspace('properties.json'))
+    pipelineJob('Applications/' + jobName) {
+        parameters {
+            propertiesFile.${jobName}.each {
+            stringParam('REGION',it.REGION,"null")
+			stringParam('AMI',it.AMI,"null")
+            }
+}
+
+        definition {
+            cps {                                                                                                     
+                script(readFileFromWorkspace("Applications/" + jobName + '/Jenkinsfile'))
+                sandbox()
+            }
+        }
+    }
+}
+
+folder('Applications') {
+}
+
+
+String JobDir = "${WORKSPACE}" + "/Applications";
+
+new File(JobDir).eachDir() { dir ->
+    println "JobPath = " + dir.getPath()
+
+    String jobName = dir.getPath().substring(dir.getPath().lastIndexOf("Applications/") + 5)
+    println "JobName " + jobName
+    create_pipeline(jobName)
+}
+
+
+
+    pipelineJob('Applications/Build_Infrastructure') {
+
+        definition {
+            cps {                                                                                                     
+                script(readFileFromWorkspace('Build_Infrastructure/Jenkinsfile'))
+                sandbox()
+            }
+        }
+    }
+	
+	
+	    pipelineJob('Skeleton_job_parallel') {
+
+
+        definition {
+            cps {                                                                                                     
+                script(readFileFromWorkspace("ParallelJenkinsfile"))
+                sandbox()
+            }
+        }
+    }
+	
+	
+//println jobName;
+//create_pipeline(jobName, "Applications/" + jobName)  
+//}
+
+//new File(JobDir).eachFile() { file->  
+//    jobName = file.getName()
+
+//def list = []
+//def dir = new File(workingDir)
+//dir.eachFile (FileType.FILES) { file ->
+//  list << file
+//}
+
+
+//list.each {
+//String jobName = it.path.substring(it.path.lastIndexOf("Applications/") + 5)
+
+//println jobName;
+//
+//}
+
+
